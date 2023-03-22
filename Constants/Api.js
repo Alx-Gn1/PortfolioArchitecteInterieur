d060a90932909e49653bc717b981f04d26283cd1
Alx-Gn1 <alex.guenin@orange.fr>
2023-03-22 15:27:20 +0100

    modif api url

--- Constants/Api.js
+++ Constants/Api.js
@@ -1,3 +1,3 @@
-const ApiUrl = "https://ec2-13-48-137-239.eu-north-1.compute.amazonaws.com:80/api";
+const ApiUrl = "https://ec2-13-48-137-239.eu-north-1.compute.amazonaws.com/api";
 
 export { ApiUrl };
